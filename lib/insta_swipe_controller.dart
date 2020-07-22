f73cbe3350b41b7d0059f944962ba0c9cd477f33
Gursheesh Singh <singhgursheesh12@gmail.com>
2020-07-23 01:25:17 +0530

    Adding instagram stories controller

---
+++ lib/insta_swipe_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class InstagramSwipeController {
+  PageController pageController;
+
+  void nextPage(
+      {duration = const Duration(milliseconds: 500), curve = Curves.linear}) {
+    pageController.nextPage(duration: duration, curve: curve);
+  }
+
+  void previousPage(
+      {duration = const Duration(milliseconds: 500), curve = Curves.linear}) {
+    pageController.previousPage(duration: duration, curve: curve);
+  }
+}
