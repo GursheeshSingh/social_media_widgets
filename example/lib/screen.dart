a4946a5d78cf6d77544b92f807d0cda48e839959
Arkane <mstr.rkn@gmail.com>
2021-04-11 23:26:27 +0000

    Add instagram story swipe with controller to example & Add 'current item index' property to InstagramSwipeController & Null safety migration

--- example/lib/screen.dart
+++ example/lib/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_widgets/insta_swipe_controller.dart';
 
 class Screen extends StatelessWidget {
   final String name;
@@ -19,6 +20,55 @@ class Screen extends StatelessWidget {
               color: Colors.white,
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ScreenWithDirectionButtons extends StatelessWidget {
+  final String name;
+  final Color color;
+  final InstagramSwipeController controller;
+
+  const ScreenWithDirectionButtons(this.name, this.color, this.controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: color,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      child: Text('Previous'),
+                      onPressed: (){
+                        controller.previousPage();
+                      }
+                  ),
+                  ElevatedButton(
+                      child: Text('Next'),
+                      onPressed: (){
+                        controller.nextPage();
+                      }
+                  )
+                ],
+              )
+            ],
+          )
         ),
       ),
     );
--- lib/insta_swipe_controller.dart
+++ lib/insta_swipe_controller.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/cupertino.dart';
 
 class InstagramSwipeController {
-  PageController pageController;
+  PageController? pageController;
+  int? currentItemIndex;
 
   void nextPage(
       {duration = const Duration(milliseconds: 500), curve = Curves.linear}) {
-    pageController.nextPage(duration: duration, curve: curve);
+    pageController!.nextPage(duration: duration, curve: curve);
   }
 
   void previousPage(
       {duration = const Duration(milliseconds: 500), curve = Curves.linear}) {
-    pageController.previousPage(duration: duration, curve: curve);
+    pageController!.previousPage(duration: duration, curve: curve);
   }
 }
